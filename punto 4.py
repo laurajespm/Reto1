3a2548bb4ef533cce3296eda1137c69c3fcafbaf
Laura Espinosa <laespinosa@unal.edu.co>
2026-04-13 09:34:52 -0500

    punto 4.py

--- punto 4.py
+++ punto 4.py
@@ -1,3 +1,4 @@
+#Se define modularmente los mensajes de bienvenida y solicitud de número
 def initial_msg():
     print("Buen día, recibo una lista de números enteros y le indico cuál es la mayor suma entre dos elementos consecutivos.")
 
@@ -11,6 +12,7 @@ def ask_num(msg):
         n=int(input(msg))
     return n
 
+#Dado que el ejercicio retorna la mayor suma entre dos elementos consecutivos, es necesario verificar que al menos se ingresen 2 números
 def create_list():
     lista=ask_num("¿Cuántos números va a ingresar? ")  
     while lista<2:
@@ -22,6 +24,8 @@ def create_list():
         li.append(num)
     return li
 
+#Se recorre la lista tomando los primeros dos elementos. Si existen una suma de otros dos números mayor que la anterior, se reeemplaza.
+#La función devuelve los dos números y su respectiva suma
 def check_sum(li):
     i=li[0]+li[1]
     num1=li[0]
@@ -36,7 +40,7 @@ def check_sum(li):
 
 def give_info(info):
     print("La mayor suma encontrada corresponde a ", info[0],", la cual resulta de los números", info[1], "y", info[2], ".")
-
+#Se agrupa todo en un main
 def main():
     initial_msg()
     li = create_list()
